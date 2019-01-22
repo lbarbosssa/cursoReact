cb7c4be61ce008fce7e78cadc6e6e68e435586e2
Lucas <barbosa.lucas1996@gmail.com>
2019-01-22 17:51:28 -0300

    Convertendo HTML para JSX e apresentando o componente criado

---
+++ company/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Site from './Site';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Site />
+      </div>
+    );
+  }
+}
+
+export default App;
