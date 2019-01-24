fc3be5e726e17b3aa6d69438a96c36425043032b
Lucas <barbosa.lucas1996@gmail.com>
2019-01-24 17:57:06 -0300

    Autenticando no Banco

--- company/src/admin/Login.js
+++ company/src/admin/Login.js
@@ -1,46 +1,59 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
-import {auth} from './../firebase-config'
+import { auth } from './../firebase-config'
 
 class Login extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
+
+        this.state = {
+            estaAutenticado: false,
+            estaLogando: false,
+            erro: false
+        }
 
         this.email = null
         this.senha = null
 
         this.autenticaUsuario = this.autenticaUsuario.bind(this)
-        
+
     }
 
-    autenticaUsuario(){
-        console.log(this.email.value, this.senha.value)
+    autenticaUsuario() {
+        this.setState({ estaLogando: true, erro: false })
         auth.signInWithEmailAndPassword(this.email.value, this.senha.value)
             .then(user => {
                 console.log('logado', user)
+                this.setState({ estaAutenticado: true })
             })
             .catch(err => {
                 console.log('Erro: ', err)
+                this.setState({ erro: true, estaAutenticado: false, estaLogando: false })
             })
     }
 
-    render(){
-        return(
+    render() {
+        if (this.state.estaAutenticado) {
+            return <Redirect to='/admin' />
+        }
+
+        return (
             <div className='container'>
                 <h1>Login</h1>
-                
-                    <div className='form-group'>
-                        <label forhtml='exampleInputEmail1'>Email address</label>
-                        <input type='email' name="email" ref={ref => this.email = ref} className='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='nome@email.com' />
-                        <small id='emailHelp' className='form-text text-muted'>We'll never share your email with anyone else.</small>
-                    </div>
-                    <div className='form-group'>
-                        <label forhtml='exampleInputPassword1'>Password</label>
-                        <input type='password' name="senha" ref={ref => this.senha = ref} className='form-control' id='exampleInputPassword1' placeholder='Senha' />
-                    </div>
-                    
-                    <button type='button' onClick={this.autenticaUsuario} className='btn btn-primary'>Submit</button>
-                
+
+                <div className='form-group'>
+                    <label forhtml='exampleInputEmail1'>Email address</label>
+                    <input type='email' name="email" ref={ref => this.email = ref} className='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='nome@email.com' />
+                </div>
+                <div className='form-group'>
+                    <label forhtml='exampleInputPassword1'>Password</label>
+                    <input type='password' name="senha" ref={ref => this.senha = ref} className='form-control' id='exampleInputPassword1' placeholder='Senha' />
+                    {this.state.erro && <small id='emailHelp' className='form-text text-muted'>Email ou senha inválidos.</small>}
+                </div>
+
+                <button type='button' disabled={this.state.estaLogando} onClick={this.autenticaUsuario} className='btn btn-primary'>Submit</button>
+
 
             </div>
         )
